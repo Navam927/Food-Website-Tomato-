080eae8bcb83daffce688b1ee6f65dee20420a70
Moksh Goyal <mokshgoyal616@gmail.com>
2024-04-18 18:37:07 +0530

    1:02:42

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import Header from "../../components/Header/Header";
+import ExploreMenu from "../ExploreMenu/ExploreMenu";
+import { useState } from "react";
+
 import "./Home.css";
 
 const Home = () => {
+  const [category, setCategory] = useState("All");
+
   return (
     <div>
       <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   );
 };
