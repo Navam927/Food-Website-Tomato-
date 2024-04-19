70f4cabbe4355297c44bc5027a6f63856655e996
Navam927 <navamsharma142@gmail.com>
2024-04-19 12:45:12 +0530

    1:38:22

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -5,8 +5,29 @@ import PropTypes from "prop-types";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+
+  const [cartItems, setCartItems] = useState({});  // Creates blank page 
+  const addToCart = (itemId) => {
+      if(cartItems[itemId]){
+        setCartItems((prev)=>({...prev,[itemId]:1}))
+      }
+      else {
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+      }
+
+      
+  }
+
+  const removeFromCart = (itemId) => {
+      setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+  }
+
   const ContextValue = {
     food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart
   };
 
   return (
