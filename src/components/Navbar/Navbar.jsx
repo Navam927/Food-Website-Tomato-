0c2d427ecc2a5e8d10f2394deef09508d73f280e
MohitSingh0707 <mohitsingh22410@gmail.com>
2024-04-27 00:25:37 +0530

    commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { assets } from "../../assets/assets";
 import "./Navbar.css";
+import { Link } from "react-router-dom"; 
+import { StoreContext } from "../../context/StoreContext";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("home");
+  const{getTotalCartAmount}=useContext(StoreContext);
 
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="" className="logo" />
+     <Link to='/'> <img src={assets.logo} alt="" className="logo" /></Link>
       <ul className="navbar-menu">
         <li
           onClick={() => setMenu("home")}
@@ -37,8 +40,8 @@ const Navbar = () => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
-          <div className="dot"></div>
+          <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
+          <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
         <button>sign in</button>
       </div>
