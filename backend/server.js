995c66e94c0d103453d5bce10cfe5ac4a698c7af
Navam <navamsharma142@gmail.com>
2025-07-12 22:43:03 +0530

    life is hard

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
-import { checkPostgresConnection } from './config/connectpost.js';
 import { connectCloudinary } from './config/cloudinary.js';
 
 
